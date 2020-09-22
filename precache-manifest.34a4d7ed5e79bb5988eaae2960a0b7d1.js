b97f90fb1520caa3cd9702a5a4a6c1606e049646
micjef <michaeljeffry4@gmail.com>
2020-09-22 19:46:06 +0700

    production version 1.0.0

---
+++ precache-manifest.34a4d7ed5e79bb5988eaae2960a0b7d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69917402322d135e0bcfbda76117247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e8346a985ad6ecdc922",
+    "url": "/static/css/main.66848aa8.chunk.css"
+  },
+  {
+    "revision": "fc840e98fb38efeef4d5",
+    "url": "/static/js/2.0177153a.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.0177153a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e8346a985ad6ecdc922",
+    "url": "/static/js/main.97f04879.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
